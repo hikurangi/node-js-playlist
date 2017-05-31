88ee0d11395f7904468f324b0cf19db26c56079e
hikurangi <hikurangi@gmail.com>
2017-05-31 22:05:24 +1200

    it works

--- video-23-27/app.js
+++ video-23-27/app.js
@@ -6,6 +6,15 @@ const port = 3000
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
+app.use('/assets', express.static(path.join(__dirname, 'assets'))) // path.join was necessary because I used custom folders
+
+// use middleware
+// app.use('/assets', (req, res, next) => {
+//   console.log(req.url);
+//   next() // all done
+// })
+
+
 
 app.get('/', (req, res) => {
   res.render('index') // express knows what to set in the headers
