ab646c7ab64f24ff4a4223f7960f80f0745d5c53
hikurangi <hikurangi@gmail.com>
2017-05-31 20:14:25 +1200

    14

--- video-11/app.js
+++ video-11/app.js
@@ -1,11 +1,21 @@
+// // initial server
 const http = require('http')
-const port = 3000
+// const port = 3000
+//
+// const server = http.createServer((req, res) => {
+//   console.log(`request was made to ${req.url}`);
+//   res.writeHead(200, { 'Content-Type': 'text/plain' })
+//   res.end('Hey ninjas')
+// })
+//
+// server.listen(port, '127.0.0.1')
+// console.log(`Server now listening on port ${port}`);
 
-const server = http.createServer((req, res) => {
-  console.log(`request was made to ${req.url}`);
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hey ninjas')
+const fs = require('fs')
+
+const myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8')
+
+myReadStream.on('data', chunk => {
+  console.log('new chunk received');
+  console.log({chunk});
 })
-
-server.listen(port, '127.0.0.1')
-console.log(`Server now listening on port ${port}`);
