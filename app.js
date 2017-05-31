2a0b25c6b91c45ec313adc4faf0bae080096820d
hikurangi <hikurangi@gmail.com>
2017-05-31 18:31:24 +1200

    7 exports

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-const counter = require('./count')
+const stuff = require('./stuff')
 
-console.log(counter(['this one', 'another one', 'the most recent one']));
+console.log(stuff.counter(['this one', 'another one', 'the most recent one']));
+console.log(stuff.adder(5, 6));
+console.log(stuff.adder(stuff.pi, 5));
