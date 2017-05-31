39d61d6b21b32ea4ff6d4254e05ca0f31b8c8fe6
hikurangi <hikurangi@gmail.com>
2017-05-31 18:25:49 +1200

    shuffling

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-require('./count')
+const counter = require('./count')
+
+console.log(counter(['this one', 'another one', 'the most recent one']));
--- count.js
+++ count.js
@@ -2,6 +2,4 @@ const counter = arr => {
   return 'There are ' + arr.length + ' elements in this array'
 }
 
-console.log(counter(['this one', 'another one', 'the most recent one']));
-
 module.exports = counter
